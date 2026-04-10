a8c69c53c600972f7e9d77de0567b5386ef07c7d
Raza Abbas <raza13abbas@gmail.com>
2026-04-10 18:15:48 +0530

    sync_fifo

--- fifo.v
+++ fifo.v
@@ -1,54 +1,65 @@
 // Code your design here
 // Code your design here
-module sync_fifo #(
-  parameter DEPTH =16,
-  parameter WIDTH = 8
-) (
-  input clk,rst,
-  
-  //write interface
-  input wr_en,
-  input wire [WIDTH-1:0] din,
-  output full,
-  
-  //read
-  input rd_en,
-  output reg [WIDTH-1:0] dout,
-  output empty
-);
-  
-  reg [WIDTH-1:0] fifo [0:DEPTH-1];
-  reg [$clog2(DEPTH):0] wr_ptr, rd_ptr;
-  
-  always @(posedge clk or posedge rst) begin
-    if (rst) begin
-      wr_ptr <= 0;
-      rd_ptr <= 0;
-      dout   <= 0;
-    end
-    else begin
-      if (wr_en && !full) begin
-        fifo[ wr_ptr[$clog2(DEPTH)-1:0] ] <= din;
-        wr_ptr <= wr_ptr + 1;
-      end
+    `timescale 1ns/1ps
 
-      if (rd_en && !empty) begin
-        dout   <= fifo[ rd_ptr[$clog2(DEPTH)-1:0] ];
-        rd_ptr <= rd_ptr + 1;
-      end
+module syn_fifo #(
+  parameter WIDTH = 8,
+  parameter DEPTH = 16
+)(
+  input  clk,
+  input  reset,
+  input  rd_en,
+  input  wt_en,
+  output reg                    fifo_full,
+  output reg                    fifo_empty,
+  input      [WIDTH-1:0]        fifo_in,
+  output     [WIDTH-1:0]        fifo_out,          
+  output reg [$clog2(DEPTH):0]  fifo_count
+);
+
+  reg [WIDTH-1:0]        mem [0:DEPTH-1];
+  reg [$clog2(DEPTH)-1:0] rd_ptr, wt_ptr;
+
+  
+  assign fifo_out = mem[rd_ptr];
+
+ 
+  always @(*) begin
+    fifo_empty = (fifo_count == 0);
+    fifo_full  = (fifo_count == DEPTH);
+  end
+
+ 
+  always @(posedge clk or posedge reset) begin
+    if (reset)
+      fifo_count <= 0;
+    else begin
+      case ({wt_en & ~fifo_full, rd_en & ~fifo_empty})
+        2'b10:   fifo_count <= fifo_count + 1;  
+        2'b01:   fifo_count <= fifo_count - 1; 
+        default: fifo_count <= fifo_count;       
+      endcase
     end
   end
 
-  assign empty = (wr_ptr == rd_ptr);
-  assign full  = ({ ~wr_ptr[$clog2(DEPTH)], wr_ptr[$clog2(DEPTH)-1:0] } == rd_ptr);
+ 
+  always @(posedge clk) begin
+    if (wt_en && !fifo_full)
+      mem[wt_ptr] <= fifo_in;
+  end
 
-  //assign full =(wr_ptr[4] != rd_ptr[4])&&(wr_ptr[3:0] == rd_ptr[3:0]);
-endmodule                                              
-                                               
-                                            
-                                              
-    
-        
-  
-        
+
+  always @(posedge clk or posedge reset) begin
+    if (reset) begin
+      wt_ptr <= 0;
+      rd_ptr <= 0;
+    end else begin
+      if (wt_en && !fifo_full)
+        wt_ptr <= wt_ptr + 1;
+      if (rd_en && !fifo_empty)
+        rd_ptr <= rd_ptr + 1;
+    end
+  end
+
+endmodule
     
